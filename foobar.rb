b078dca15ee046ae07eb9ace2cf90be17b97dd2f
Kevin Pham <kephams@berkeley.edu>
2017-09-14 15:41:48 -0700

    hw1 and foobar

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,13 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map! { |x| x.to_i }
+    a.map! { |x| x + 2}
+    a = a.select { |x| x%2 ==0}
+  	a = a.uniq
+  	a = a.select { |x| x<=10}
+  	a = a.inject(0){|sum, x| sum + x}
+  	return a
   end
 end
 
